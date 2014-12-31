f33e8291ce3b8239a9bbdcef70024acdd78a9f6e
Silence <yb_17136@163.com>
2014-12-31 12:30:40 +0800

    this is a new project

---
+++ src/pac/Main.java
@@ -0,0 +1,30 @@
+package pac;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+	public static void main(String str[]) {
+		String s[] = { "app_key", "session", "id", "comment", "sign",
+				"comment_id" };
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("app_key", "ak");
+		map.put("session", "se");
+		map.put("id", "i");
+		map.put("comment", "c");
+		map.put("sign", "si");
+		map.put("comment_id", "ci");
+		String ss[] = new String[map.size()];
+		int i = 0;
+		for (String key : map.keySet()) {
+			ss[i] = key + "=" + map.get(key);
+			i++;
+		}
+		Arrays.sort(ss);
+		for (String st : ss) {
+			System.out.println(st);
+		}
+	}
+
+}
